0f47e8d651fa9c624cb3867bfb5824644a9360dc
youta0922 <t24t153zm@ous.jp>
2024-11-29 14:53:24 +0900

    leap_year_algorithm.dart

--- leap_year_algorithm/leap_year_algorithm.dart
+++ leap_year_algorithm/leap_year_algorithm.dart
@@ -1,6 +1,23 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main({
+// 西暦を入力してうるう年かどうかを判定するプログラム
+
+ is_leap_year(year):
+if (year % 4 == 0){
+  return True
 }
-// Test change for pull request
-// Added a new comment
-// New test change
+if (year % 100 == 0 and year % 400 == 0):
+return True
+else:
+return False
+else:
+return True
+else:
+return False
+
+year = 2024
+result = is_leap_year(year)
+result
+
+ },
+)
+async 
